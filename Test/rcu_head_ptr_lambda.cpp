8aa628ce586f9a115536fcb95e78c12c35a4f4d7
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2016-08-23 14:10:05 -0700

    Pass a lambda expression to call_rcu()
    Woo-hoo!  My very first C++ lambda expression!!!
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

---
+++ Test/rcu_head_ptr_lambda.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <unistd.h>
+#include "urcu-signal.hpp"
+#include "rcu_head_ptr.hpp"
+
+// Added-pointer approach
+
+struct foo {
+	int a;
+	class std::rcu_head_ptr<struct foo> rh;
+};
+
+struct foo foo1;
+
+int main(int argc, char **argv)
+{
+	class std::rcu_head_ptr<struct foo> *rhp = &foo1.rh;
+
+	foo1.a = 42;
+	foo1.rh = &foo1;
+	std::call_rcu(rhp,
+		      [] (struct std::rcu_head *rhp) {
+				struct foo *fp = fp->rh.enclosing_class(rhp);
+
+				std::cout << "Callback fp->a: " << fp->a << "\n";
+		      });
+
+	std::rcu_barrier();
+
+	return 0;
+}
