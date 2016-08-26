d18a4d475e7d97ba25aea50d0cb68514fc24ffc9
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2016-08-26 10:47:51 -0700

    Make the rcu_head_ptr example more C++-like
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- Test/rcu_head_ptr.cpp
+++ Test/rcu_head_ptr.cpp
@@ -6,6 +6,10 @@
 // Added-pointer approach
 
 struct foo {
+	foo(int i)
+	{
+		this->a = i;
+	}
 	int a;
 	class std::rcu_head_ptr<struct foo> rh;
 };
@@ -17,20 +21,17 @@ void my_cb(struct std::rcu_head *rhp)
 	std::cout << "Callback fp->a: " << fp->a << "\n";
 }
 
-struct foo foo1;
+struct foo foo1(42);
 
 int main(int argc, char **argv)
 {
-	class std::rcu_head_ptr<struct foo> *rhp = &foo1.rh;
-
-	foo1.a = 42;
 	foo1.rh = &foo1;
-	std::call_rcu(rhp, my_cb);
+	std::call_rcu(&foo1.rh, my_cb);
 	std::rcu_barrier();
 
-	foo1.a = 43;
+	foo1 = 43;
 	foo1.rh = &foo1;
-	std::call_rcu(rhp,
+	std::call_rcu(&foo1.rh,
 		      [] (struct std::rcu_head *rhp) {
 		      	struct foo *fp = fp->rh.enclosing_class(rhp);
 
