a23c9ff6f5f757f20c0eebd255b257a6c9857659
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2016-08-26 12:09:52 -0700

    Make rcu_head_container_of use a static member for rh_offset
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- Test/rcu_head_container_of.hpp
+++ Test/rcu_head_container_of.hpp
@@ -11,15 +11,18 @@ namespace std {
 			T t;
 			T *p = &t;
 
-			this->rh_offset = ((char *)&(p->*rh_field)) - (char *)p;
+			rh_offset = ((char *)&(p->*rh_field)) - (char *)p;
 		}
 
 		T *enclosing_class(struct rcu_head *rhp)
 		{
-			return (T *)((char *)rhp - this->rh_offset);
+			return (T *)((char *)rhp - rh_offset);
 		}
 
 	private:
-		size_t rh_offset;
+		static size_t rh_offset;
 	};
+
+	template<typename T>
+	size_t rcu_head_container_of<T>::rh_offset;
 }
