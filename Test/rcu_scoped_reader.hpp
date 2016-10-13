e6f0b762a51293fbf3d8426b1db1a3354226581d
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2016-10-13 04:42:22 -0700

    Apply Arthur O'Dwyer feedback
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- Test/rcu_scoped_reader.hpp
+++ Test/rcu_scoped_reader.hpp
@@ -1,13 +1,13 @@
 namespace std {
 	class rcu_scoped_reader {
 	public:
-		rcu_scoped_reader()
+		rcu_scoped_reader() noexcept
 		{
 			this->rd = nullptr;
 			rcu_read_lock();
 		}
 
-		rcu_scoped_reader(class rcu_domain *rd)
+		explicit rcu_scoped_reader(class rcu_domain *rd) noexcept
 		{
 			this->rd = rd;
 			rd->read_lock();
@@ -17,7 +17,7 @@ namespace std {
 		
 		rcu_scoped_reader&operator=(const rcu_scoped_reader &) = delete;
 
-		~rcu_scoped_reader()
+		~rcu_scoped_reader() noexcept
 		{
 			if (this->rd)
 				this->rd->read_unlock();
