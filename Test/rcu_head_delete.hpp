26fe68a583e24ce0ecbe808c3184a4b14a1d78bb
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2016-11-11 11:55:37 -0800

    Prevent segv from rcu_head_delete
    This fix is crude: It simply creates a static member function wrapping
    the delete operator, and uses that instead of "{}" for the default
    deleter.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- Test/rcu_head_delete.hpp
+++ Test/rcu_head_delete.hpp
@@ -18,13 +18,18 @@ namespace std {
 			rhdp->deleter(obj);
 		}
 
-		void call(D d = {})
+		static void rcu_head_deleter(T *obj)
+		{
+			delete(obj);
+		}
+
+		void call(D d = rcu_head_deleter)
 		{
 			deleter = d;
 			call_rcu(static_cast<rcu_head *>(this), trampoline);
 		}
 
-		void call(rcu_domain &rd, D d = {})
+		void call(rcu_domain &rd, D d = rcu_head_deleter)
 		{
 			deleter = d;
 			rd.call(static_cast<rcu_head *>(this), trampoline);
@@ -41,12 +46,17 @@ namespace std {
 			D()(obj);
 		}
 
-		void call(D d = {})
+		static void rcu_head_deleter(T *obj)
+		{
+			delete(obj);
+		}
+
+		void call(D d = rcu_head_deleter)
 		{
 			call_rcu(static_cast<rcu_head *>(this), trampoline);
 		}
 
-		void call(rcu_domain &rd, D d = {})
+		void call(rcu_domain &rd, D d = rcu_head_deleter)
 		{
 			rd.call(static_cast<rcu_head *>(this), trampoline);
 		}
