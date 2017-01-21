30fcb9296056c2636c7a3a36a2273e8c8ad1d311
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2017-01-21 15:03:16 -0800

    ajodwyer/: Add "std::rcu::indirect" to match David's "std::rcu_ptr".
    And update "test1.cpp" to perform the same test cases on both
    implementations without repeating TOO much code. Hooray!

--- Test/test1.cpp
+++ Test/test1.cpp
@@ -1,4 +1,11 @@
-#include "rcu_ptr.hpp"
+#if IMP_DSHOLLMAN
+ #include "rcu_ptr.hpp"
+#elif IMP_AJODWYER
+ #include "urcu-signal.hpp"
+ #include "rcu_indirect.hpp"
+ #include <functional>
+#endif
+
 #include <assert.h>
 
 int Foo_destructions;
@@ -15,7 +22,11 @@ void test_default_retire()
     Foo_destructions = 0;
     Foo *pf = new Foo(42);
     {
+#if IMP_DSHOLLMAN
         std::experimental::rcu_ptr<Foo> fp(pf);
+#elif IMP_AJODWYER
+        std::rcu::indirect<Foo> fp(pf);
+#endif
         fp.retire();  // defers a call to "delete pf"
     }
     assert(Foo_destructions == 0);
@@ -30,7 +41,11 @@ void test_lambda_retire()
     int j = 5;
     int calls = 0;
     {
+#if IMP_DSHOLLMAN
         std::experimental::rcu_ptr<Foo> fp(&f);
+#elif IMP_AJODWYER
+        std::rcu::indirect<Foo, std::function<void(Foo*)>> fp(&f);
+#endif
         fp.retire([j, &calls](Foo* fval) {
             assert(fval->a == 42);
             assert(j == 5);
@@ -48,7 +63,11 @@ void test_default_destructor()
     Foo_destructions = 0;
     Foo f(42);
     {
+#if IMP_DSHOLLMAN
         std::experimental::rcu_ptr<Foo> fp(&f);
+#elif IMP_AJODWYER
+        std::rcu::indirect<Foo> fp(&f);
+#endif
         // destroying the rcu_ptr does not retire or destroy f itself
     }
     assert(Foo_destructions == 0);
@@ -63,10 +82,15 @@ void test_lambda_destructor()
     int j = 5;
     int calls = 0;
     {
-        std::experimental::rcu_ptr<Foo> fp(&f, [j, &calls](Foo* fval) {
+        auto lambda = [j, &calls](Foo* fval) {
             ++calls;
             assert(false);
-        });
+        };
+#if IMP_DSHOLLMAN
+        std::experimental::rcu_ptr<Foo> fp(&f, std::move(lambda));
+#elif IMP_AJODWYER
+        std::rcu::indirect<Foo, std::function<void(Foo*)>> fp(&f, std::move(lambda));
+#endif
         // destroying the rcu_ptr does not call the lambda
     }
     assert(Foo_destructions == 0 && calls == 0);
@@ -81,12 +105,17 @@ void test_lambda_destructor_plus_retire()
     int j = 5;
     int calls = 0;
     {
-        std::experimental::rcu_ptr<Foo> fp(&f, [j, &calls](Foo* fval) {
+        auto lambda = [j, &calls](Foo* fval) {
             assert(fval->a == 42);
             assert(j == 5);
             assert(calls == 0);
             ++calls;
-        });
+        };
+#if IMP_DSHOLLMAN
+        std::experimental::rcu_ptr<Foo> fp(&f, std::move(lambda));
+#elif IMP_AJODWYER
+        std::rcu::indirect<Foo, std::function<void(Foo*)>> fp(&f, std::move(lambda));
+#endif
         fp.retire();  // defers a call to the lambda above
     }
     assert(Foo_destructions == 0 && calls == 0);
