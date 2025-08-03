1074665ead46e583768a759301149c391d6697e9
Paul E. McKenney <paulmck@kernel.org>
2025-08-03 14:05:47 -0700

    testrw.cpp: Add assertions
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- Test/paulmck/testrw.cpp
+++ Test/paulmck/testrw.cpp
@@ -3,9 +3,10 @@
 #include <shared_mutex>
 #include <syncstream>
 #include <thread>
+#include <cassert>
 
 std::shared_mutex myrwlock;
-int a, b; // Current configuration values
+int a = 1, b = 7; // Current configuration values
 
 void my_get(int *cur_a, int *cur_b)
 {
@@ -23,10 +24,15 @@ void my_set(int new_a, int new_b)
 
 int main(int argc, char *argv[])
 {
-	int my_a, my_b;
+	int my_a;
+	int my_b;
 
+	my_get(&my_a, &my_b);
+	printf("a = %d, b = %d\n", my_a, my_b);
+	assert(my_a == 1 && my_b == 7);
 	my_set(3, 4);
 	my_get(&my_a, &my_b);
 	printf("a = %d, b = %d\n", my_a, my_b);
+	assert(my_a == 3 && my_b == 4);
 	return 0;
 }
